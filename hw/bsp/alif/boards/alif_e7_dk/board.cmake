db3bcafafcf5c95d111e809011cf4b6e5df2afb0
Michael Koiev <koiev.mykhailo@alifsemi.com>
2025-06-23 11:15:45 +0300

    build: remove redundant linker script

--- hw/bsp/alif/boards/alif_e7_dk/board.cmake
+++ hw/bsp/alif/boards/alif_e7_dk/board.cmake
@@ -5,10 +5,5 @@ else()
   set(MCU_VARIANT M55_HE)
 endif()
 
-# Define custom linker script for board
-set(BOARD_LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/alif_e7.ld)
-
-message(STATUS "Using BOARD_LINKER_SCRIPT: ${BOARD_LINKER_SCRIPT}")
-
 function(update_board TARGET)
 endfunction()
