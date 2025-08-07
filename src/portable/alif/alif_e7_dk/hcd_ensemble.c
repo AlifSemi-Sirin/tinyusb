2ea304b9afa0bb9aa81a44ff6870ed8bb0129ac9
Dmitry Savinkin <dsavinkin@gmail.com>
2025-08-07 15:51:24 +0300

    Added xhci host driver sources.

---
+++ src/portable/alif/alif_e7_dk/hcd_ensemble.c
@@ -0,0 +1,234 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023 Ha Thach (tinyusb.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * This file is part of the TinyUSB stack.
+ */
+
+#include "tusb_option.h"
+
+#if CFG_TUH_ENABLED && CFG_TUSB_MCU == OPT_MCU_NONE
+
+#include "host/hcd.h"
+#include "ux_hcd_xhci_api.h"
+//#include "dcd_ensemble_def.h" //for gobal USB registers
+
+#include "clk.h"
+#include "power.h"
+
+#if 1
+// USB Registers Access Types
+#define _rw volatile uint32_t
+#define __w volatile uint32_t
+#define __r volatile const uint32_t
+
+volatile struct {
+    union {
+        _rw gctl;                       // Global Core Control Register
+        struct {
+            _rw dsblclkgtng      : 1;   // Disable clock gating
+            __r gblhibernationen : 1;   // Hibernation enable status at the global level
+            __r : 1;                    // Reserved
+            _rw disscramble      : 1;   // Disable scrambling
+            _rw scaledown        : 2;   // Scale-down mode
+            _rw ramclksel        : 2;   // RAM clock select
+            __r : 2;                    // Reserved
+            _rw sofitpsync       : 1;   // Reserved
+            _rw coresoftreset    : 1;   // Core soft reset
+            _rw prtcapdir        : 2;   // Port capability direction
+            _rw frmscldwn        : 2;   // Frame scales down
+            __r : 1;                    // Reserved
+            _rw bypssetaddr      : 1;   // Bypass set address in Device mode
+            __r : 14;                   // Reserved
+        } gctl_b;
+    };
+} *host_ugbl = (void *) (USB_BASE + 0xC110);
+#endif
+
+//--------------------------------------------------------------------+
+// Controller API
+//--------------------------------------------------------------------+
+
+// optional hcd configuration, called by tuh_configure()
+bool hcd_configure(uint8_t rhport, uint32_t cfg_id, const void* cfg_param) {
+  (void) rhport;
+  (void) cfg_id;
+  (void) cfg_param;
+
+  return false;
+}
+
+// Initialize controller to host mode
+bool hcd_init(uint8_t rhport, const tusb_rhport_init_t* rh_init) {
+  (void) rhport;
+  (void) rh_init;
+
+  static UX_HCD hcd;
+  uint32_t ret;
+
+  // enable 20mhz clock
+  enable_cgu_clk20m();
+  // enable usb peripheral clock
+  enable_usb_periph_clk();
+  // power up usb phy
+  enable_usb_phy_power();
+  // disable usb phy isolation
+  disable_usb_phy_isolation();
+  // clear usb phy power-on-reset signal
+  CLKCTL_PER_MST->USB_CTRL2 &= ~(1 << 8);
+
+  sys_busy_loop_us(50000);
+  host_ugbl->gctl_b.coresoftreset = 1;
+  sys_busy_loop_us(50000);
+  host_ugbl->gctl_b.prtcapdir = 0x1; //Host mode
+  sys_busy_loop_us(50000);
+  host_ugbl->gctl_b.coresoftreset = 0;
+  sys_busy_loop_us(50000);
+
+
+  hcd.ux_hcd_io = (void*)USB_BASE;
+
+  ret = _ux_hcd_xhci_initialize(&hcd);
+
+  return ret == UX_SUCCESS;
+}
+
+uint32_t USB_IRQHandler_cnt = 0;
+// Interrupt Handler
+void hcd_int_handler(uint8_t rhport, bool in_isr) {
+  (void) rhport;
+  (void) in_isr;
+  USB_IRQHandler_cnt++;
+  _ux_xhci_event_irq_handler(hcd_xhci);
+}
+
+// Enable USB interrupt
+void hcd_int_enable (uint8_t rhport) {
+  (void) rhport;
+}
+
+// Disable USB interrupt
+void hcd_int_disable(uint8_t rhport) {
+  (void) rhport;
+}
+
+// Get frame number (1ms)
+uint32_t hcd_frame_number(uint8_t rhport) {
+  (void) rhport;
+  return 0;
+}
+
+//--------------------------------------------------------------------+
+// Port API
+//--------------------------------------------------------------------+
+
+// Get the current connect status of roothub port
+bool hcd_port_connect_status(uint8_t rhport) {
+  (void) rhport;
+  return false;
+}
+
+// Reset USB bus on the port. Return immediately, bus reset sequence may not be complete.
+// Some port would require hcd_port_reset_end() to be invoked after 10ms to complete the reset sequence.
+void hcd_port_reset(uint8_t rhport) {
+  (void) rhport;
+}
+
+// Complete bus reset sequence, may be required by some controllers
+void hcd_port_reset_end(uint8_t rhport) {
+  (void) rhport;
+}
+
+// Get port link speed
+tusb_speed_t hcd_port_speed_get(uint8_t rhport) {
+  (void) rhport;
+  return TUSB_SPEED_FULL;
+}
+
+// HCD closes all opened endpoints belong to this device
+void hcd_device_close(uint8_t rhport, uint8_t dev_addr) {
+  (void) rhport;
+  (void) dev_addr;
+}
+
+//--------------------------------------------------------------------+
+// Endpoints API
+//--------------------------------------------------------------------+
+
+// Open an endpoint
+bool hcd_edpt_open(uint8_t rhport, uint8_t dev_addr, tusb_desc_endpoint_t const * ep_desc) {
+  (void) rhport;
+  (void) dev_addr;
+  (void) ep_desc;
+
+  // NOTE: ep_desc is allocated on the stack when called from usbh_edpt_control_open()
+  // You need to copy the data into a local variable who maintains the state of the endpoint and transfer.
+  // Check _hcd_data in hcd_dwc2.c for example.
+
+  return false;
+}
+
+bool hcd_edpt_close(uint8_t rhport, uint8_t daddr, uint8_t ep_addr) {
+  (void) rhport; (void) daddr; (void) ep_addr;
+  return false; // TODO not implemented yet
+}
+
+// Submit a transfer, when complete hcd_event_xfer_complete() must be invoked
+bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t * buffer, uint16_t buflen) {
+  (void) rhport;
+  (void) dev_addr;
+  (void) ep_addr;
+  (void) buffer;
+  (void) buflen;
+
+  return false;
+}
+
+// Abort a queued transfer. Note: it can only abort transfer that has not been started
+// Return true if a queued transfer is aborted, false if there is no transfer to abort
+bool hcd_edpt_abort_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr) {
+  (void) rhport;
+  (void) dev_addr;
+  (void) ep_addr;
+
+  return false;
+}
+
+// Submit a special transfer to send 8-byte Setup Packet, when complete hcd_event_xfer_complete() must be invoked
+bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet[8]) {
+  (void) rhport;
+  (void) dev_addr;
+  (void) setup_packet;
+
+  return false;
+}
+
+// clear stall, data toggle is also reset to DATA0
+bool hcd_edpt_clear_stall(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr) {
+  (void) rhport;
+  (void) dev_addr;
+  (void) ep_addr;
+
+  return false;
+}
+
+#endif
